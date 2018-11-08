b8928828ba0dfc8253117b50115479030f172a45
zlp <1131808958@qq.com>
2018-11-08 16:51:19 +0800

    yarn add styled-components

---
+++ src/jianshu/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Header from './common/header'
+
+class JianShu extends Component{
+  render() {
+    return(
+      <div>
+        <Header></Header>
+      </div>
+    )
+  }
+}
+
+export default JianShu
--- src/style/index.js
+++ src/style/index.js
@@ -1,4 +1,8 @@
-@keyframes hide-item {
+import { injectGlobal } from "styled-components";
+
+//全局样式
+injectGlobal`
+  @keyframes hide-item {
   0% {
     opacity: 1;
   }
@@ -13,5 +17,6 @@
 
 .hide {
   /* forwards保存最后一帧动画样式 */
-  animation: hide-item 2s ease-in forwards
+  animation: hide-item 2s ease-in forwards;
 }
+`
